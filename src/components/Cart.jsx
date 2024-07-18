1df75ee1c999dcf6c7b819f6a740eba600fce021
Rameshkanuri95 <74371283+Rameshkanuri95@users.noreply.github.com>
2024-07-18 13:38:39 +0530

    import remove

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import Loading from "./Loading"
 import Product from "./Product"
-import useProducts from "../hooks/useProducts"
+
 
 const Cart = () => {
   const [product, setProduct] = useState([])
